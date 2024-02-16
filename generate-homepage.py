a73c930119f79e98416e0fb5cf69d40f310d9872
Jothin Kumar <contact@jothin.tech>
2024-02-16 10:26:53 +0530

    url slug

--- generate-homepage.py
+++ generate-homepage.py
@@ -1,19 +1,10 @@
 new_app = lambda name, url: f'<li><a href="{url}">{name}</a></li>'
 
 apps = [
-    {"name": "qw", "url": "https://joth.in"}, 
-    {"name": "ert", "url": "https://joth.in"}, 
-    {"name": "yuiop", "url": "https://joth.in"}, 
-    {"name": "as", "url": "https://joth.in"}, 
-    {"name": "df", "url": "https://joth.in"}, 
-    {"name": "gh", "url": "https://joth.in"}, 
-    {"name": "jkl", "url": "https://joth.in"}, 
-    {"name": "zxc", "url": "https://joth.in"}, 
-    {"name": "vbn", "url": "https://joth.in"}, 
-    {"name": "m", "url": "https://joth.in"}, 
+    {"name": "qw", "url-slug": "qw"}, 
 ]
 
 with open("index.html", "r") as html_file:
     html_content = html_file.read()
 with open("index.html", "w") as html_file:
-    html_file.write(html_content.replace("<!-- apps-here -->", "\n".join([new_app(data["name"], data["url"]) for data in apps])))
+    html_file.write(html_content.replace("<!-- apps-here -->", "\n".join([new_app(data["name"], "/" + data["url-slug"]) for data in apps])))
